c5d40a19df6d4d72e25aed66be1483dce6929e91
wltan <wltan@users.noreply.github.com>
2020-02-07 12:00:47 +0800

    Switch game location
    Location may still be subject to change from time to time

--- src/components/academy/game/index.tsx
+++ src/components/academy/game/index.tsx
@@ -4,7 +4,7 @@ export class Game extends React.Component {
 
   public render() {
     // Consider making this an environment variable
-    const GAME_LOCATION = "https://source-academy-game.github.io/prototype-sandbox/quickstart/";
+    const GAME_LOCATION = "https://source-academy-game.github.io/prototype-sandbox/prototype/Source Academy/";
     return (
       <div style={{backgroundColor: "black", width: "100%", height: "100%"}}>
         <iframe src={GAME_LOCATION} width="100%" height="100%"/>
